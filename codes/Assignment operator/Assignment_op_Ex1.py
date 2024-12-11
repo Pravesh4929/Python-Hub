627ed9c84911a8de66022e2fafd7342d8d04ddd6
Pravesh4929 <mrpraveshmishra786@gmail.com>
2024-12-11 10:37:31 +0530

    commit 3

---
+++ codes/Assignment operator/Assignment_op_Ex1.py
@@ -0,0 +1,13 @@
+#Write a Python Program which will swap Two Values--Logic1
+#AssignmentOpEx1.py
+#This Program works for Swappping of Any Type of Values
+a,b=input("Enter Value of a:"),input("Enter Value of b:")
+print("-----------------------------")
+print("Original Value of a={}".format(a))
+print("Original Value of b={}".format(b))
+print("-----------------------------")
+#Swapping Logic
+a,b=b,a # Multi line assignment
+print("Swapped Value of a={}".format(a))
+print("Swapped Value of b={}".format(b))
+print("-----------------------------")
